d4a33b389dacdc1d6c8f74060033296c701709dd
Brent Van de Reyd <91150116+BrentVandeReyd@users.noreply.github.com>
2022-11-16 21:19:24 +0100

    Update and rename randomizer.py to faker.py

---
+++ app/faker.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+from faker import Faker
+from faker.providers import internet
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost/",
+    "http://localhost:8080/",
+    "https://localhost.tiangolo.com/",
+    "http://127.0.0.1:5500/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
+
+@app.get("/internet")
+async def root():
+    i=0
+    fake = Faker()
+    fake.add_provider(internet)
+    return {fake.ipv4_private()}
+
+@app.get("/japan")
+async def root():
+    fake = Faker(['ja_JP'])
+    for i in range(10):
+        return {fake.name()}
+
+@app.get("/Amerikaan")
+async def root():
+    fake = Faker(['en_US'])
+    for i in range(10):
+        return {fake.name()}
+
+class japan(BaseModel):
+    name: str
+
+@app.post("/japan/", response_model=japan)
+async def Japan(jap: japan):
+    return jap
+
+# uvicorn test:app --reload
